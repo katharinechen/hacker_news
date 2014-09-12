c9e9327e5a56d0c0b257c10301904ff05523aa9f
Katharine Chen <katharinechen.ny@gmail.com>
2014-09-12 10:15:49 -0700

    Add seed.rb with random created_by generator

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -12,11 +12,19 @@ class VotesController < ApplicationController
 
   def create
     @vote = Vote.new(vote_params)
-    if @vote.save
-      flash[:notice] = "Thank you for submitting a vote."
+    if (Vote.find(current_user.id) == []) && (current_user.id != @vote.link.user_id)
+      if @vote.save
+        flash[:notice] = "Thank you for submitting a vote."
+        redirect_to root_url
+      end
+    else
+      flash[:notice] = "Your vote has not been submitted. Users can only vote once and for links that they did not submit."
       redirect_to root_url
     end
   end
+
+  # if (the vote saved) && (the current user is not in the vote table) &&
+  #(the current_user is not the author)
 
 private
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+katharinechen = User.create(email: "katharinechen.ny@gmail.com", password: "1234")
+bobmiller = User.create(email: "bobmiller@gmail.com", password: "45678")
+
+link1 = Link.create(url: "http://www.theonion.com/articles/biden-gets-grow-light-delivered-to-white-house-und,36597/", user_id: "1")
+
+link2 = Link.create(url: "http://www.theonion.com/articles/concert-security-drastically-overestimating-fans-d,36900/", user_id: "2")
+link3 = Link.create(url: "http://www.theonion.com/articles/college-football-player-first-in-family-to-meet-nc,36841/", user_id: "2")
+link4 = Link.create(url: "http://www.theonion.com/articles/vikings-intrigued-by-matt-cassels-ability-to-get-t,36807/", user_id: "1")
+
+Link.all.each do |link|
+  link.update_attribute :created_at, (rand*10).days.ago
+end
