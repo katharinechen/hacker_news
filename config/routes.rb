1d821241d23f25f1a15f3884f368ab3f8e7792cb
Katharine Chen <katharinechen.ny@gmail.com>
2014-09-26 19:49:42 -0700

    Delete unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
 
-	root :to => 'links#index'
+	root :to => 'links#popular'
 
 	get 'signup', to: 'users#new', as: 'signup'
 	get 'login', to: 'sessions#new', as: 'login'
 	get 'logout', to: 'sessions#destroy', as: 'logout'
   get 'popular', to: 'links#popular' 
 
-  resources :users
+  resources :users, except: [:index, :destroy]
   resources :sessions
-  resources :votes
+  resources :votes, except: [:index, :edit, :update, :show, :destroy]
 
-  resources :links do
+  resources :links, except: [:edit, :update, :destroy] do
     resources :comments
   end
 end
